fb339f72733f28de8f026f83e52e147894438cf2
SKane <le_cas315@hotmail.com>
2020-02-05 12:57:27 +1000

    dp.py fix 0.0.1

--- cogs/dpy/dp.py
+++ cogs/dpy/dp.py
@@ -2,15 +2,18 @@
 async def dp(ctx, projectname):
     await ctx.channel.purge(limit=1)
     channels = ['test']
+    role = get(ctx.guild.roles, name=f"{projectname} Dev")
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
 
-    await ctx.role.delete(role = get(ctx.guild.roles, name=f"{projectname} Dev"))
+    
+    await ctx.role.delete()
 
-    channels = get(ctx.guild.text_channels, name=i,category=category)
-    await channels.delete()
+    for i in channels:
+        channel = get(ctx.guild.text_channels, name=i, category=category)
+        await ctx.channel.delete()
 
-    await get(ctx.guild.voice_channels, name='chat', category=category), category=category).delete()
-
+    await ctx.get(ctx.guild.voice_channels, name='chat', category=category), category=category).delete()
+    
     await ctx.category.delete()
 
     await ctx.channel.send(f"Category {projectname} deleted!")
