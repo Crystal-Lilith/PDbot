7851e86fe2b5b6828f59a0fb548e074fe09ffaf4
SKane <le_cas315@hotmail.com>
2020-02-05 14:58:12 +1000

    dpy fix 0.0.7

--- cogs/dpy/cp.py
+++ cogs/dpy/cp.py
@@ -2,22 +2,30 @@
 async def cp(ctx, projectname):
     await ctx.channel.purge(limit=1)
     channels = ['sources', 'to-do-list', 'suggestions', 'known-bugs', 'general', 'bot-commands']
-    userPerms = [
-        
-    ]
-    botPerms = [
-        
-    ]
 
+    await ctx.guild.create_role(name=f"{projectname} Founder", mentionable=True)
     await ctx.guild.create_role(name=f"{projectname} Dev", mentionable=True)
-    role = get(ctx.guild.roles, name=f"{projectname} Dev")
-    await ctx.message.author.add_roles(role)
+    founder = get(ctx.guild.roles, name=f"{projectname} Founder")
+    dev = get(ctx.guild.roles, name=f"{projectname} Dev")
+    await ctx.message.author.add_roles(founder)
 
     bot = get(ctx.guild.roles, name='PDBot')
 
     overwrites = {
         ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        role: discord.PermissionOverwrite(manage_channels = False,
+        founder: discord.PermissionOverwrite(manage_channels = False,
+                                        read_messages = True,
+                                        view_channel = True,
+                                        send_messages = True,
+                                        send_tts_messages = True,
+                                        manage_messages = True,
+                                        embed_links = True,
+                                        attach_files = True,
+                                        read_message_history = True,
+                                        manage_roles = False,
+                                        manage_permissions = False,
+                                        manage_webhooks = True),
+        dev: discord.PermissionOverwrite(manage_channels = False,
                                         read_messages = True,
                                         view_channel = True,
                                         send_messages = True,
@@ -46,8 +54,6 @@ async def cp(ctx, projectname):
 
     await ctx.guild.create_category(f"{projectname} Dev", overwrites=overwrites)
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
-    # await category.set_permissions(role, userPerms)
-    # await category.set_permissions(bot, botPerms)
 
 
     for i in channels:
--- cogs/dpy/dp.py
+++ cogs/dpy/dp.py
@@ -2,22 +2,22 @@
 async def dp(ctx, projectname):
     await ctx.channel.purge(limit=1)
     channels = ['sources', 'to-do-list', 'suggestions', 'known-bugs', 'general', 'bot-commands']
-    role = get(ctx.guild.roles, name=f"{projectname} Dev")
+    founder = get(ctx.guild.roles, name=f"{projectname} Founder")
+    dev = get(ctx.guild.roles, name=f"{projectname} Dev")
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
     
-    # if ctx.CategoryChannel == category:
-    await role.delete()
+    if get(ctx.author.roles, name=f"{projectname} Founder"):
+        await founder.delete()
+        await dev.delete()
 
-    for i in channels:
-        channel = get(ctx.guild.text_channels, name=i, category=category)
-        await channel.delete()
+        for i in channels:
+            channel = get(ctx.guild.text_channels, name=i, category=category)
+            await channel.delete()
 
-    await get(ctx.guild.voice_channels, name='chat', category=category).delete()
-    
-    await category.delete()
+        await get(ctx.guild.voice_channels, name='chat', category=category).delete()
+        
+        await category.delete()
 
-    await ctx.author.send(f"Category **{projectname}** deleted!", delete_after=60)
-    # else:
-    #     ctx.message.send('Please use this command in the category of the project you are trying to delete under **__bot-commands__**!')
-
-
+        await ctx.author.send(f"Category **{projectname}** deleted!", delete_after=60)
+    else:
+        ctx.message.send('You must be the __founder__ of the project to delete it!')
