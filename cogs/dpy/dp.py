b5d57a7937fa0807030cb350b6059fabbd083767
SKane <le_cas315@hotmail.com>
2020-02-05 14:44:39 +1000

    dp.py fix 0.0.8

--- cogs/dpy/dp.py
+++ cogs/dpy/dp.py
@@ -5,19 +5,19 @@ async def dp(ctx, projectname):
     role = get(ctx.guild.roles, name=f"{projectname} Dev")
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
     
-    if ctx.CategoryChannel == category:
-        await role.delete()
+    # if ctx.CategoryChannel == category:
+    await role.delete()
 
-        for i in channels:
-            channel = get(ctx.guild.text_channels, name=i, category=category)
-            await channel.delete()
+    for i in channels:
+        channel = get(ctx.guild.text_channels, name=i, category=category)
+        await channel.delete()
 
-        await get(ctx.guild.voice_channels, name='chat', category=category).delete()
-        
-        await category.delete()
+    await get(ctx.guild.voice_channels, name='chat', category=category).delete()
+    
+    await category.delete()
 
-        await ctx.author.send(f"Category **{projectname}** deleted!", delete_after=60)
-    else:
-        ctx.message.send('Please use this command in the category of the project you are trying to delete under **__bot-commands__**!')
+    await ctx.author.send(f"Category **{projectname}** deleted!", delete_after=60)
+    # else:
+    #     ctx.message.send('Please use this command in the category of the project you are trying to delete under **__bot-commands__**!')
 
 
