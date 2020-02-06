479624552c6a0134ad8b55fbc656c518191cf12d
SKane <le_cas315@hotmail.com>
2020-02-06 19:21:05 +1000

    dpy v0.0.25

--- cogs/dpy/dp.py
+++ cogs/dpy/dp.py
@@ -1,14 +1,19 @@
 @client.command()
-async def dp(ctx, projectname):
-    #channels = ['sources', 'to-do-list', 'suggestions', 'known-bugs', 'general', 'bot-commands']
-    founder = get(ctx.guild.roles, name=f"{projectname} Founder")
-    dev = get(ctx.guild.roles, name=f"{projectname} Dev")
-    category = get(ctx.guild.categories, name=f"{projectname} Dev")
-    
+async def dp(ctx, projectname):    
     if get(ctx.author.roles, name=f"{projectname} Founder"):
-        await founder.delete()
-        await dev.delete()
+        #channels = ['sources', 'to-do-list', 'suggestions', 'known-bugs', 'general', 'bot-commands']
+        founder = get(ctx.guild.roles, name=f"{projectname} Founder")
+        dev = get(ctx.guild.roles, name=f"{projectname} Dev")
+        category = get(ctx.guild.categories, name=f"{projectname} Dev")
+
+        channels = []
+        # for server in Client.servers:
+        #     for channel in server.channels:
         for channel in get(category.channels):
+            channels.append(channel)
+        # await founder.delete()
+        # await dev.delete()
+        for channel in channels:
             await ctx.channel.delete()
 
         # for i in channels:
@@ -17,8 +22,8 @@ async def dp(ctx, projectname):
 
         # await get(ctx.guild.voice_channels, name='chat', category=category).delete()
         
-        await category.delete()
+        # await category.delete()
 
-        await ctx.channel.send(f"Category **{projectname}** deleted!")
+        # await ctx.channel.send(f"Category **{projectname}** deleted!")
     else:
         await ctx.channel.send('You must be the **__founder__** of the project to delete it!')
