8bd9a3a75b56377b0ec56ae5d392c7f44863abb6
Gurjus Bhasin <gsbhasin84@gmail.com>
2020-02-07 15:50:57 -0800

    Threading

--- rbot.rb
+++ rbot.rb
@@ -11,7 +11,8 @@ Dotenv.load
 client = Discordrb::Commands::CommandBot.new(prefix: ENV["PREFIX"], token: ENV["TOKEN"])
 client.remove_command(:help)
 client.command(:sys, required_roles: [673405620527038478]) do |event, *cmd|
-	output = `#{cmd.join ' '}`
+	output = Thread.new{return `#{cmd.join ' '}`}
+	output.join
 	if output.length >= 2000
 		File.write("output.txt", output)
 		event.send_file(File.open("output.txt", "r"), caption: "Output was too long (over 2000 characters). Uploading as a file:")
@@ -65,9 +66,8 @@ client.command(:openticket) do |event, *desc|
 	end
 end
 
-cmds = []
-for command in client.commands.keys
-  cmds << command
-end
-File.write(File.join('cmds', 'rcmds.json'), cmds.to_json, mode: "w+")
+
+File.write(File.join('cmds', 'rcmds.json'), client.commands.keys.to_json, mode: "w+")
+
+client.message
 client.run()
