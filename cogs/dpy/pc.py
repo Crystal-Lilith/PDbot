7fa89fafd6006293f03d200627a36839a141b173
SKane <le_cas315@hotmail.com>
2020-02-08 20:43:45 +1000

    pc.py v0.0.4

--- cogs/dpy/pc.py
+++ cogs/dpy/pc.py
@@ -2,13 +2,15 @@
 async def pc(ctx, _hex, *, projectname):
     projectname = projectname.lower()
     _hex = _hex.replace('#', '')
-    color = discord.Color.from_rgb(tuple(int(_hex[i:i+2], 16) for i in (0, 2, 4)))
-    founder = get(ctx.guild.roles, name=f"{projectname} Founder")
-    dev = get(ctx.guild.roles, name=f"{projectname} Dev")
+    _hex = tuple(int(_hex[i:i+2], 16) for i in (0, 2, 4))
+    await ctx.channel.send(_hex)
+    # color = discord.Color.from_rgb(_hex)
+    # founder = get(ctx.guild.roles, name=f"{projectname} Founder")
+    # dev = get(ctx.guild.roles, name=f"{projectname} Dev")
 
-    if get(ctx.author.roles, name=f"{projectname} Founder"):
-        await founder.edit(color=color)
-        await dev.edit(color=color)
-        await ctx.channel.send(f"The project color has been updated to **{hex}**!")
-    else:
-        await ctx.channel.send('You must be the **__founder__** of the project to change the color!')
+    # if get(ctx.author.roles, name=f"{projectname} Founder"):
+    #     await founder.edit(color=color)
+    #     await dev.edit(color=color)
+    #     await ctx.channel.send(f"The project color has been updated to **{hex}**!")
+    # else:
+    #     await ctx.channel.send('You must be the **__founder__** of the project to change the color!')
