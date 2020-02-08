a8a3fc689123ce3fe0dbba1f58975c7d83ff60ef
SKane <le_cas315@hotmail.com>
2020-02-08 12:41:35 +1000

    ad v0.0.2

--- cogs/dpy/ad.py
+++ cogs/dpy/ad.py
@@ -5,16 +5,16 @@ async def ad(ctx, user: discord.User, *, projectname):
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
 
     if get(ctx.author.roles, name=f"{projectname} Founder"):
-        if user == None:
-            await ctx.channel.send("That **user** does not exist!")
+        # if user == None:
+        #     await ctx.channel.send("That **user** does not exist!")
+        # else:
+        if category == None:
+            await ctx.channel.send("That **project** does not exist!")
         else:
-            if category == None:
-                await ctx.channel.send("That **project** does not exist!")
+            if get(user.roles, name=f"{projectname} Dev") == None:
+                await user.add_roles(get(ctx.guild.roles, name=f"{projectname} Dev"))
+                await ctx.channel.send(f"User **{user}** added to **{projectname} Dev**!")
             else:
-                if get(user.roles, name=f"{projectname} Dev") == None:
-                    await user.add_roles(get(ctx.guild.roles, name=f"{projectname} Dev"))
-                    await ctx.channel.send(f"User **{user}** added to **{projectname} Dev**!")
-                else:
-                    await ctx.channel.send(f"User **{user}** is already a dev of **{projectname}**!")
+                await ctx.channel.send(f"User **{user}** is already a dev of **{projectname}**!")
     else:
         await ctx.channel.send('You must be the **__founder__** of the project to add devs!')
