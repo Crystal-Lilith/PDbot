02175780192fb09f30b648c46194f173f75aa193
SKane <le_cas315@hotmail.com>
2020-02-08 22:38:20 +1000

    dbot.py v0.0.5

--- dbot.py
+++ dbot.py
@@ -26,7 +26,7 @@ async def on_ready():
 async def help(ctx):
     cmds = {}
     for command in client.commands:
-        cmds[command.name] = f"desc:{{{discord.ext.commands.command.description}}},"
+        cmds[command.name] = "No description" # f"desc:{{{discord.ext.commands.command.description}}},"
     with open('./cmds/dcmds.json', 'w+') as f:
         json.dumps(cmds, f)
 
