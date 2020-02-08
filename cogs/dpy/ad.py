c88889e286c42d1deeecbae579b0f6c503b60df1
SKane <le_cas315@hotmail.com>
2020-02-08 13:04:54 +1000

    ad.py v0.0.6

--- cogs/dpy/ad.py
+++ cogs/dpy/ad.py
@@ -1,7 +1,7 @@
 @client.command()
 async def ad(ctx, user, *, projectname):
     projectname = projectname.lower()
-    user = get(ctx.guild.members, name=user.replace('@', ''))
+    user = get(ctx.guild.members, name=(user.replace('@', '')))
     category = get(ctx.guild.categories, name=f"{projectname} Dev")
 
     if get(ctx.author.roles, name=f"{projectname} Founder"):
