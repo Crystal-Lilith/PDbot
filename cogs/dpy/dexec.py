d0689bcdb741f0aaa835fbe8ba534e15f5ccbdc9
SKane <le_cas315@hotmail.com>
2020-02-08 21:55:15 +1000

    dexec.py v0.0.1

---
+++ cogs/dpy/dexec.py
@@ -0,0 +1,13 @@
+from hata import enter_executor
+
+@client.has_role()
+@client.command()
+async def dexec(ctx, *code):
+    output=exec(code)
+    if len(output) > 2000:
+        async with enter_executor():
+            with open('output.txt') as f:
+                f.write(output)
+        await ctx.send(content='Output was too long, here is the output in a file', file='output.txt')
+    else:
+        await ctx.send(output)
