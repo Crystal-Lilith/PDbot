82bcc7487b4d5ea72d53aafcba8fb7af9f69fa1f
SKane <le_cas315@hotmail.com>
2020-02-08 20:32:48 +1000

    pc, prc v0.0.3

--- cogs/dpy/pc.py
+++ cogs/dpy/pc.py
@@ -1,12 +1,13 @@
 @client.command()
-async def pc(ctx, red, green, blue, *, projectname):
+async def pc(ctx, hex, *, projectname):
     projectname = projectname.lower()
-    color = discord.Color.from_rgb(int(red), int(green), int(blue))
+    color = discord.Color.from_rgb(tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
     founder = get(ctx.guild.roles, name=f"{projectname} Founder")
     dev = get(ctx.guild.roles, name=f"{projectname} Dev")
 
     if get(ctx.author.roles, name=f"{projectname} Founder"):
         await founder.edit(color=color)
         await dev.edit(color=color)
+        await ctx.channel.send(f"The project color has been updated to **{hex}**!")
     else:
         await ctx.channel.send('You must be the **__founder__** of the project to change the color!')
--- cogs/dpy/prc.py
+++ cogs/dpy/prc.py
@@ -8,5 +8,6 @@ async def prc(ctx, *, projectname):
     if get(ctx.author.roles, name=f"{projectname} Founder"):
         await founder.edit(color=color)
         await dev.edit(color=color)
+        await ctx.channel.send(f"The project color has been updated to **{color}**!")
     else:
         await ctx.channel.send('You must be the **__founder__** of the project to change the color!')
