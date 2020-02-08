b1642ba07816627f00e26bd765774fd52f61d630
SKane <le_cas315@hotmail.com>
2020-02-08 21:10:44 +1000

    rp.py v0.0.1

---
+++ cogs/dpy/rp.py
@@ -0,0 +1,18 @@
+@client.command()
+async def rp(ctx, newprojectname, *, projectname):
+    newprojectname = newprojectname.lower()
+    projectname = projectname.lower()
+    if get(ctx.guild.categories, name=f"{newprojectname} Dev") == None:
+        if get(ctx.author.roles, name=f"{projectname} Founder"):
+            founder = get(ctx.guild.roles, name=f"{projectname} Founder")
+            dev = get(ctx.guild.roles, name=f"{projectname} Dev")
+            category = get(ctx.guild.categories, name=f"{projectname} Dev")
+
+            founder.edit(name=f"{newprojectname} Founder")
+            dev.edit(f"{newprojectname} Dev")
+            category.edit(f"{newprojectname} Dev")
+        else:
+            await ctx.channel.send('You must be the **__founder__** of the project to change the project name!')
+
+    else:
+        await ctx.channel.send("That project name already exists!")
