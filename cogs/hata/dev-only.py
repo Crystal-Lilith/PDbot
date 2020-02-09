57aea0d5dfadda4b446440a17e97df8550b19e65
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-02-09 15:17:29 +0000

    Update dev-only.py

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -7,4 +7,4 @@ commands = eventlist()
 async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
         async with enter_executor():
-            client.loop.create_task_threadsafe(client.message_create(message.channel, getoutput("curl --quiet icanhazip.com")))
+            client.loop.create_task_threadsafe(client.message_create(message.channel, getoutput("curl --silent icanhazip.com")))
