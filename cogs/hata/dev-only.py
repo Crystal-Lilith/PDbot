3b1b92a0b0e013e02dde1459916996cd1ded02d2
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-02-09 15:05:33 +0000

    Update dev-only.py

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -1,9 +1,9 @@
 from hata import eventlist
-import subprocess.getoutput
+from subprocess import getoutput
 
 commands = eventlist()
 
 @commands
 async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
-        await client.message_create(message.channel, subprocess.getoutput("curl icanhazip.com"))
+        await client.message_create(message.channel, getoutput("curl icanhazip.com"))
