4783a09852c13dcf4bb70a9e1d6075b704ac25eb
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2020-02-12 15:56:55 +0000

    Update ad.py

--- cogs/dpy/ad.py
+++ cogs/dpy/ad.py
@@ -1,4 +1,4 @@
-@client.command(description=str({"desc":"Adds a dev to your project", "required_roles":None, "required_perms":None}))
+@client.command(description=json.dumps({"desc":"Adds a dev to your project", "required_roles":None, "required_perms":None}))
 async def ad(ctx, user: discord.User, *, projectname):
     projectname = projectname.lower()
     user = ctx.guild.get_member(user.id)
