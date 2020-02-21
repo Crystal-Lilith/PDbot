5157c6b26b2243955b0e7d11b8ac33afb80f5250
SKane <le_cas315@hotmail.com>
2020-02-21 16:59:21 +1000

    help test

--- hbot.py
+++ hbot.py
@@ -69,47 +69,47 @@ async def restart_bot(client, message):
     await client.message_create(message.channel, "Updating bot...")
     os.system('sh stop.sh')
 
-@on_command
-async def help(client, message, content):
-    if content not in [None, '']:
-        return
-    cmds = []
-    for i in pdbot.events.message_create.commands:
-        cmds.append(i)
-    for y in os.listdir('./cmds/'):
-        with open('./cmds/'+y, 'r') as f:
-            x = json.load(f)
-        for i in x:
-            if i.lower() != 'help':
-                cmds.append(i)
-    cmds = sorted(cmds, key=str.lower) # Key positional argument is used to make sure uppercase commands don't take precedence.
-    pages=[]
-    part=[]
-    index=0
-    for element in cmds:
-        if index==16:
-            pages.append('\n'.join(part))
-            part.clear()
-            index=0
-        part.append(f'**>>** {element}')
-        index+=1
+# @on_command
+# async def help(client, message, content):
+#     if content not in [None, '']:
+#         return
+#     cmds = []
+#     for i in pdbot.events.message_create.commands:
+#         cmds.append(i)
+#     for y in os.listdir('./cmds/'):
+#         with open('./cmds/'+y, 'r') as f:
+#             x = json.load(f)
+#         for i in x:
+#             if i.lower() != 'help':
+#                 cmds.append(i)
+#     cmds = sorted(cmds, key=str.lower) # Key positional argument is used to make sure uppercase commands don't take precedence.
+#     pages=[]
+#     part=[]
+#     index=0
+#     for element in cmds:
+#         if index==16:
+#             pages.append('\n'.join(part))
+#             part.clear()
+#             index=0
+#         part.append(f'**>>** {element}')
+#         index+=1
 
-    pages.append('\n'.join(part))
+#     pages.append('\n'.join(part))
 
-    del part
+#     del part
 
-    result=[]
+#     result=[]
 
-    limit=len(pages)
-    index=0
-    while index<limit:
-        embed=Embed('Commands:',color='029320',description=pages[index])
-        index+=1
-        embed.add_field("Do $meme for some funny memes!", f'page {index}/{limit}')
-        result.append(embed)
+#     limit=len(pages)
+#     index=0
+#     while index<limit:
+#         embed=Embed('Commands:',color='029320',description=pages[index])
+#         index+=1
+#         embed.add_field("Do $meme for some funny memes!", f'page {index}/{limit}')
+#         result.append(embed)
 
-    del pages
-    await Pagination(client, message.channel,result)
+#     del pages
+#     await Pagination(client, message.channel,result)
 
 app=Flask(__name__)
 
