993f29dba3dbc9f9ed02010a0b2ede10d5e11c16
SKane <le_cas315@hotmail.com>
2020-02-21 17:01:28 +1000

    dcmds debug

--- dbot.py
+++ dbot.py
@@ -29,6 +29,7 @@ async def help(ctx):
     with open('./cmds/dcmds.json', 'w') as f:
         for command in client.commands:
             cmds[command.name] = json.loads(command.description)
+            await ctx.channel.send(cmds)
         json.dump(cmds, f)
         f.close()
 
