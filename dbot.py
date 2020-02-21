5b0673b1396a69390b22b139cf70da335b6b4f5b
SKane <le_cas315@hotmail.com>
2020-02-21 17:18:02 +1000

    dcmds fix

--- dbot.py
+++ dbot.py
@@ -15,7 +15,7 @@ except:
 client = commands.Bot(command_prefix=os.environ.get('PREFIX'), case_insensitive=True, description='PDBot - v 0.9.0', 
                       status=discord.Status.idle, activity=discord.Game(name='Compiling'))
 
-client.remove_command('help') # hata and D.rb handle that
+client.remove_command('help')
 
 
 @client.event
@@ -25,10 +25,10 @@ async def on_ready():
 @client.command()
 async def help(ctx):
     cmds = {}
-    with open('./cmds/dcmds.json', 'w+') as f:
+    open('./cmds/dcmds.json', 'w').close()
+    with open('./cmds/dcmds.json', 'w') as f:
         for command in client.commands:
-            cmds[command.name] = json.loads(command.description)
-            asyncio.sleep(0.1)
+            cmds[command.name] = {"desc": command.description, 'required_roles': [], 'required_perms': []}
             json.dump(cmds, f)
         f.close()
 
