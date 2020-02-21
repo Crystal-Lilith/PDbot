3a25cc22f9c92d3a2eff83c882e7b42de36cc169
SKane <le_cas315@hotmail.com>
2020-02-21 16:33:14 +1000

    dcmds fix

--- dbot.py
+++ dbot.py
@@ -28,7 +28,7 @@ async def help(ctx):
     with open('./cmds/dcmds.json', 'w+') as f:
         for command in client.commands:
             cmds[command.name] = command.description
-            json.dump(json.loads(cmds), f)
+            json.dump(str(json.loads(cmds)), f)
         f.close()
 
 for i in os.listdir('./cogs/dpy'):
