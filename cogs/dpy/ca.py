1c75a5119d3744fc2204b5dcdd8dbbe9276ff421
SKane <le_cas315@hotmail.com>
2020-02-23 02:25:26 +1000

    ca.py beta fix

--- cogs/dpy/ca.py
+++ cogs/dpy/ca.py
@@ -1,6 +1,6 @@
 @client.command(description='Creates announcement')
 @commands.has_role('Owner')
-async def ac(ctx, option, title, *, message):
+async def ca(ctx, option, title, *, message):
     announce = get(ctx.guild.channels, name='📢server-announcements')
     await ctx.announce.embed(title=f'**+**[{title}]', color=discord.Color.from_rgb(0, 191, 255), description=message)
     await ctx.channel.send('Announcement posted!')
