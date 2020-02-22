111370bc65f976950f96d8a00a5833b88e1e16a6
SKane <le_cas315@hotmail.com>
2020-02-23 02:38:06 +1000

    ca.py beta fix v0.0.1

--- cogs/dpy/ca.py
+++ cogs/dpy/ca.py
@@ -3,6 +3,6 @@
 async def ca(ctx, option, title, *, message):
     announce = get(ctx.guild.channels, name='📢server-announcements')
     embed = discord.Embed(title=f'**+**[{title}]', color=discord.Color.from_rgb(0, 191, 255), description=message)
-    embed.set_author(ctx.message.author)
+    embed.set_author(name=ctx.message.author)
     await announce.send(embed=embed)
     await ctx.channel.send('Announcement posted!')
