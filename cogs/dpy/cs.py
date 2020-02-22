6627decd1dff50e242d7ff499cc75d98008b5848
SKane <le_cas315@hotmail.com>
2020-02-23 03:12:39 +1000

    cs.py v0.0.3

--- cogs/dpy/cs.py
+++ cogs/dpy/cs.py
@@ -1,5 +1,11 @@
 @client.command(description='Changes bot status')
 @commands.has_role('PDBot Dev')
-async def cs(ctx, *, status):
-    await client.change_presence(activity=discord.CustomActivity(name=status))
-    await ctx.channel.send('Bot status changed!')
+async def cs(ctx, status, *, desc):
+    if status == 'game' or status == 'stream':
+        if status == 'game':
+            await client.change_presence(activity=discord.Game(name=desc))
+        if status == 'stream':
+            await client.change_presence(activity=discord.Streaming(name=desc))
+        await ctx.channel.send('Bot status changed!')
+    else:
+        print('Not a valid status!')
