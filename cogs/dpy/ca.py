5eadb888c4d00a9210c95857e10bea54394d2e06
SKane <le_cas315@hotmail.com>
2020-02-23 02:54:12 +1000

    ca.py beta v0.0.5

--- cogs/dpy/ca.py
+++ cogs/dpy/ca.py
@@ -3,13 +3,13 @@
 async def ca(ctx, option, title, *, message):
     if option == 'general' or option == 'add' or option == 'remove' or option == 'modify':
         if option == 'general':
-            embed = discord.Embed(title=title, color=discord.Color.from_rgb(0, 191, 255), description=message)
+            embed = discord.Embed(title=f'{title} :tada:', color=discord.Color.from_rgb(0, 191, 255), description=message)
         if option == 'add':
-            embed = discord.Embed(title=f'**+**[{title}]', color=discord.Color.from_rgb(0, 255, 0), description=message)
+            embed = discord.Embed(title=f'**+**[{title}] :tada:', color=discord.Color.from_rgb(0, 255, 0), description=message)
         if option == 'remove':
-            embed = discord.Embed(title=f'**-**[{title}]', color=discord.Color.from_rgb(178, 34, 34), description=message)
+            embed = discord.Embed(title=f'**-**[{title}] :tada:', color=discord.Color.from_rgb(178, 34, 34), description=message)
         if option == 'modify':
-            embed = discord.Embed(title=f'**/**[{title}]', color=discord.Color.from_rgb(255, 255, 51), description=message)
+            embed = discord.Embed(title=f'**/**[{title}] :tada:', color=discord.Color.from_rgb(255, 255, 51), description=message)
         announce = get(ctx.guild.channels, name='📢server-announcements')
         embed.set_footer(text=f'Author: {ctx.message.author}')
         await announce.send(embed=embed)
