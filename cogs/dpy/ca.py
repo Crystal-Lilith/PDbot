10b67c630c432d6e7d74b3fd90b730cacfbc9556
SKane <le_cas315@hotmail.com>
2020-02-23 02:51:02 +1000

    ca.py beta v0.0.4

--- cogs/dpy/ca.py
+++ cogs/dpy/ca.py
@@ -1,8 +1,18 @@
 @client.command(description='Creates announcement')
 @commands.has_role('Owner')
 async def ca(ctx, option, title, *, message):
-    announce = get(ctx.guild.channels, name='📢server-announcements')
-    embed = discord.Embed(title=f'**+**[{title}]', color=discord.Color.from_rgb(0, 191, 255), description=message)
-    embed.set_footer(text=f'Author: {ctx.message.author}')
-    await announce.send(embed=embed)
-    await ctx.channel.send('Announcement posted!')
+    if option == 'general' or option == 'add' or option == 'remove' or option == 'modify':
+        if option == 'general':
+            embed = discord.Embed(title={title}, color=discord.Color.from_rgb(0, 191, 255), description=message)
+        if option == 'add':
+            embed = discord.Embed(title=f'**+**[{title}]', color=discord.Color.from_rgb(0, 255, 0), description=message)
+        if option == 'remove':
+            embed = discord.Embed(title=f'**-**[{title}]', color=discord.Color.from_rgb(178, 34, 34), description=message)
+        if option == 'modify':
+            embed = discord.Embed(title=f'**/**[{title}]', color=discord.Color.from_rgb(255, 255, 51), description=message)
+        announce = get(ctx.guild.channels, name='📢server-announcements')
+        embed.set_footer(text=f'Author: {ctx.message.author}')
+        await announce.send(embed=embed)
+        await ctx.channel.send('Announcement posted!')
+    else:
+        await ctx.channel.send('Invalid option!')
