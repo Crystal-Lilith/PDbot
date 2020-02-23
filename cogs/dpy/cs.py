7e8eb55555263a7d55f50600216ca27318bd5d7e
SKane <le_cas315@hotmail.com>
2020-02-23 17:47:58 +1000

    cs dynamic update

--- cogs/dpy/cs.py
+++ cogs/dpy/cs.py
@@ -1,5 +1,14 @@
 @client.command(description='Changes bot status')
 @commands.has_role('PDBot Dev')
-async def cs(ctx, *, desc):
-    await client.change_presence(activity=discord.Game(name=desc))
-    await ctx.channel.send('Bot status changed!')
+async def cs(ctx, mode, *, desc):
+    if mode == 'static' or mode == 'dynamic':
+        if mode == 'static':
+            await client.change_presence(activity=discord.Game(name=desc))
+            await ctx.channel.send('Bot status changed!')
+        if mode == 'dynamic':
+            desc = desc.split()
+            await ctx.channel.send('Bot status changed!')
+            while True:
+                for i in desc:
+                    await client.change_presence(activity=discord.Game(name=i))
+                    asyncio.sleep(10)
