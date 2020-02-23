d3a69d1936aa4bc95734687ea838ec67f829434d
SKane <le_cas315@hotmail.com>
2020-02-23 18:02:22 +1000

    cs fix dynamic

--- cogs/dpy/cs.py
+++ cogs/dpy/cs.py
@@ -8,8 +8,10 @@ async def cs(ctx, mode, *, desc):
         if mode == 'dynamic':
             desc = desc.split()
             await ctx.channel.send('Bot status changed!')
-            while True:
-                for i in desc:
-                    await client.change_presence(activity=discord.Game(name=i))
-                    await ctx.channel.send(i)
-                    asyncio.sleep(50)
+            client.loop.create_task(status_task(desc))
+        
+async def status_task(desc):
+    while True:
+        for i in desc:
+            await client.change_presence(activity=discord.Game(name=i)))
+            await asyncio.sleep(1)
