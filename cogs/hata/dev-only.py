88eee0109e3a2223a9d06d649bf399693d0db22b
codic <gsbhasin84@gmail.com>
2020-02-23 01:23:16 +0000

    Update 'cogs/hata/dev-only.py'

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -6,6 +6,5 @@ commands = eventlist()
 @commands
 async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
-      	client.message_create(message.channel, gethostbyname(gethostname()))
-        #async with enter_executor():
-            #client.loop.create_task_threadsafe(client.message_create(message.channel, gethostbyname(gethostname())))
+        async with enter_executor():
+            client.loop.create_task_threadsafe(client.message_create(message.channel, gethostbyname(gethostname())))
