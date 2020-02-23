f99bacfde51feca82f96d7c3141879145f2ce555
codic <gsbhasin84@gmail.com>
2020-02-23 01:30:58 +0000

    Update 'cogs/hata/dev-only.py'

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -7,5 +7,4 @@ commands = eventlist()
 async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
         async with enter_executor():
-          	ip = urllib2.urlopen('http://icanhazip.org')
-            client.loop.create_task_threadsafe(client.message_create(message.channel, f"Bot's public IP is {ip}"))
+            client.loop.create_task_threadsafe(client.message_create(message.channel, f"Bot's public IP is {urllib2.urlopen('http://icanhazip.org')}"))
