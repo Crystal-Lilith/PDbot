1823720897a2b628dd9fe7e8137690c40b8f4576
codic <gsbhasin84@gmail.com>
2020-02-23 01:20:42 +0000

    Update 'cogs/hata/dev-only.py'

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -1,10 +1,11 @@
 from hata import eventlist, enter_executor
 from subprocess import getoutput
-
+from socket import gethostbyname, gethostname
 commands = eventlist()
 
 @commands
 async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
+      	client.message_create(message.channel, gethostbyname(gethostname())
         async with enter_executor():
             client.loop.create_task_threadsafe(client.message_create(message.channel, getoutput("curl --silent icanhazip.com")))
