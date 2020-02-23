21db32317daf4809d54b75d8dc261698ceb9d1d3
codic <gsbhasin84@gmail.com>
2020-02-23 01:23:03 +0000

    Update 'cogs/hata/dev-only.py'

--- cogs/hata/dev-only.py
+++ cogs/hata/dev-only.py
@@ -8,4 +8,4 @@ async def ip(client, message):
     if message.author.id in [562086061153583122, 300126997718237195, 207188318130012160, 524288464422830095]:
       	client.message_create(message.channel, gethostbyname(gethostname()))
         #async with enter_executor():
-            #client.loop.create_task_threadsafe(client.message_create(message.channel, getoutput("curl --silent icanhazip.com")))
+            #client.loop.create_task_threadsafe(client.message_create(message.channel, gethostbyname(gethostname())))
