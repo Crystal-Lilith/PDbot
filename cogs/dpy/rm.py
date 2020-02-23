aa0b2e684c4c8efaa78abc5d072b017cbb70b040
SKane <le_cas315@hotmail.com>
2020-02-23 18:58:33 +1000

    rm.py v0.0.4

--- cogs/dpy/rm.py
+++ cogs/dpy/rm.py
@@ -5,10 +5,10 @@ async def rm(ctx, amount):
         amount = int(amount)
         embed = discord.Embed(title=f'Success!', color=discord.Color.from_rgb(0, 255, 0),
                                 description=f'Deleted {amount} messages!')
-        embed.set_footer(text=f'Author: {ctx.message.author}')
+        embed.set_footer(text=f'By: {ctx.message.author}')
         await ctx.channel.purge(limit=(amount + 1))
         await ctx.channel.send(embed=embed, delete_after=10)
     except:
         embed = discord.Embed(title=f'Error!', color=discord.Color.from_rgb(178, 34, 34), description='Amount must be an integer!')
-        embed.set_footer(text=f'Author: {ctx.message.author}')
+        embed.set_footer(text=f'By: {ctx.message.author}')
         await ctx.channel.send(embed=embed)
