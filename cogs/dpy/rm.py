77193ec989d89a7278474bfdf56a33d3e228cdfc
SKane <le_cas315@hotmail.com>
2020-02-23 18:43:25 +1000

    rm.py v0.0.1

---
+++ cogs/dpy/rm.py
@@ -0,0 +1,9 @@
+@client.command(description='Deletes messages in a channel')
+@commands.has_role('Owner')
+async def rm(ctx, amount):
+    try:
+        int(amount)
+        ctx.channel.purge(amount+1)
+        await ctx.channel.send(f'Deleted {amount+1} messages!', delete_after=10)
+    except:
+        await ctx.channel.send('Amount must be an integer!')
