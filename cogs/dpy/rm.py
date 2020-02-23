3700fb28c4dbbd4743f1d95e6dd48bd6a606c313
SKane <le_cas315@hotmail.com>
2020-02-23 18:47:39 +1000

    rm.py debug

--- cogs/dpy/rm.py
+++ cogs/dpy/rm.py
@@ -1,9 +1,9 @@
 @client.command(description='Deletes messages in a channel')
 @commands.has_role('Owner')
 async def rm(ctx, amount):
-    try:
-        amount = int(amount) + 1
-        await ctx.channel.purge(amount)
-        await ctx.channel.send(f'Deleted {amount} messages!', delete_after=10)
-    except:
-        await ctx.channel.send('Amount must be an integer!')
+    # try:
+    amount = int(amount) + 1
+    await ctx.channel.purge(amount)
+    await ctx.channel.send(f'Deleted {amount} messages!', delete_after=10)
+    # except:
+    #     await ctx.channel.send('Amount must be an integer!')
