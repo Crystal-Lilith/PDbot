8787eae4b2a90e0122cfe9f3ab891c26c249381d
SKane <le_cas315@hotmail.com>
2020-02-26 20:44:04 +1000

    cs.py sleep fix

--- cogs/dpy/cs.py
+++ cogs/dpy/cs.py
@@ -24,4 +24,4 @@ async def status_task(desc):
     while True:
         for i in desc:
             await client.change_presence(activity=discord.Game(name=i))
-            await asyncio.sleep(1.5)
+            await asyncio.sleep(1.7)
