ae3eb3ff2f5cf16c04faa831797f23cbba9264c3
SKane <le_cas315@hotmail.com>
2020-02-27 16:56:26 +1000

    ls.py fix attempt

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -1,6 +1,6 @@
-# @client.command(description='Shows you all files in the specified directory')
-# @commands.has_any_role('PDBot Mod', 'PDBot Dev')
-# async def ls(ctx, *, directory):
+@client.command(description='Shows you all files in the specified directory')
+@commands.has_any_role('PDBot Mod', 'PDBot Dev')
+async def ls(ctx, *, directory):
 #     # for x in directory.lower().split('/'):
 #     #     if x == '.env' or if x == 'start.sh':
 #     #         return
@@ -10,5 +10,6 @@
 #         break
 #     # for i in os.listdir(directory):
 #     #     x = f"{x}{i}\n"
-#     embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```{f[:-1]}```')
-#     await ctx.channel.send(embed=embed)
+    embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```{os.listdir('.')[:-1]}```')
+    await ctx.channel.send(embed=embed)
+
