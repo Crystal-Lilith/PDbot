d5a70a1df6dad25426e48187ba57da5ff24a1351
SKane <le_cas315@hotmail.com>
2020-02-27 17:39:47 +1000

    cv.py beta fix

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -4,5 +4,5 @@ async def cv(ctx, timer, *, desc):
     embed = discord.Embed(title=f'Vote [Timer: {timer}]', color=discord.Color.from_rgb(0, 255, 0), description=message)
     embed.set_footer(text=f'Author: {ctx.message.author}')
     vote = await ctx.channel.send(embed=embed)
-    for i in [':white_check_mark:', ':no_entry_sign:']:
+    for i in ['✅', '⛔']:
         await vote.add_reaction(i)
