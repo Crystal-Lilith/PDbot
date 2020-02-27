8a01bcdc2e89aea958071850c944220012f25d67
Proxy <proxneon960@hotmail.com>
2020-02-27 19:16:28 +0000

    Update 'cogs/dpy/ls.py'

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -5,5 +5,5 @@ async def ls(ctx, *, directory):
     for i in os.listdir(directory):
         if x.lower() not in [".env", "start.sh"]:
             x = f"{x}{i}\n"
-    embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
+    embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
     await ctx.channel.send(embed=embed)
