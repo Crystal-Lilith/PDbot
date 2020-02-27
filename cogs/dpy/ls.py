b0be5ea0b819f001d8b11362e7f4d1d3deab8fd5
SKane <le_cas315@hotmail.com>
2020-02-27 16:04:58 +1000

    ls.py beta v0.0.1

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -1,12 +1,11 @@
 @client.command(description='Shows you all files in the specified directory')
 @commands.has_any_role('PDBot Mod', 'PDBot Dev')
-async def ls(ctx, directory):
+async def ls(ctx, *, directory):
     for x in directory.lower().split('/'):
         if i in ['.env', 'start.sh']:
             return
-    x=''
     for i in listdir(directory):
         x = f"{x}{i}\n"
     embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```{x[:-1]}```')
     await ctx.channel.send(embed=embed)
-    del(x, i, embed)
+    del embed
