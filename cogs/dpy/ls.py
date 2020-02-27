66fa1a9f40687505a140e66f54393a201891d68e
Proxy <proxneon960@hotmail.com>
2020-02-27 19:13:19 +0000

    Update 'cogs/dpy/ls.py'

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -1,15 +1,8 @@
-# @client.command(description='Shows you all files in the specified directory')
-# @commands.has_any_role('PDBot Mod', 'PDBot Dev')
-# async def ls(ctx, *, directory):
-#     for x in directory.lower().split('/'):
-#         if x == '.env' or if x == 'start.sh':
-#             return
-#     f = []
-#     for (dirpath, dirnames, filenames) in walk(directory):
-#         f.extend(filenames)
-#         break
-#     for i in os.listdir(directory):
-#         x = f"{x}{i}\n"
-#     files = os.listdir('.')[:-1]
-#     embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```{}```')
-#     await ctx.channel.send(embed=embed)
+@client.command(description='Shows you all files in the specified directory')
+@commands.has_any_role('PDBot Mod', 'PDBot Dev')
+async def ls(ctx, *, directory):
+    for i in os.listdir(directory):
+        if x.lower() not in [".env", "start.sh"]:
+            x = f"{x}{i}\n"
+    embed = discord.Embed(title=f'List of files in {directory}', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
+    await ctx.channel.send(embed=embed)
