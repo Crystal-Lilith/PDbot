d2cd34d114304ff3af450c0a31e02e03bd4b24da
SKane <le_cas315@hotmail.com>
2020-02-27 17:41:46 +1000

    cv.py test

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -1,8 +1,8 @@
 @client.command(description='Creates a vote')
 @commands.has_role('Owner')
 async def cv(ctx, timer, *, desc):
-    embed = discord.Embed(title=f'Vote [Timer: {timer}]', color=discord.Color.from_rgb(0, 255, 0), description=message)
+    embed = discord.Embed(title=f'Vote [Timer: {timer}]', color=discord.Color.from_rgb(0, 255, 0), description=desc)
     embed.set_footer(text=f'Author: {ctx.message.author}')
     vote = await ctx.channel.send(embed=embed)
-    for i in ['✅', '⛔']:
+    for i in [':white_check_mark:', ':no_entry_sign:']:
         await vote.add_reaction(i)
