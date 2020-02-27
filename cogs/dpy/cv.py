6782ed21bce9053b0c0bf7ff9e1026ecb1d50f52
SKane <le_cas315@hotmail.com>
2020-02-27 18:29:58 +1000

    cv send fix

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -27,6 +27,5 @@ async def cv(ctx, timer, *, desc):
 
     embed = discord.Embed(title=f'Vote #{vote_amount}', color=discord.Color.from_rgb(0, 255, 0), description='Vote ended!')
     embed.set_footer(text=f'Ended the vote by: {ctx.message.author}')
-    for i in vote.reactions:
-        await ctx.channel.send(i)
+    await ctx.channel.send(vote.reactions)
     await ctx.channel.send(embed=embed)
