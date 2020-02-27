d9882f669982c35915afa51ad2600f28920c5197
SKane <le_cas315@hotmail.com>
2020-02-27 19:13:08 +1000

    cv.py object location fix

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -26,7 +26,7 @@ async def cv(ctx, timer, *, desc):
     vote = await ctx.channel.fetch_message(vote.id)
     await asyncio.sleep(float(timer) * 60)
 
-    await ctx.channel.send(vote.reactions.count)
+    await ctx.channel.send(vote.reactions.count())
     embed = discord.Embed(title=f'Vote #{vote_amount}', color=discord.Color.from_rgb(0, 255, 0), description='Vote ended!')
     embed.set_footer(text=f'Ended the vote by: {ctx.message.author}')
     await ctx.channel.send(embed=embed)
