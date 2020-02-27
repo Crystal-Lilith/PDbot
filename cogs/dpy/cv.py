c45d6685e359cc7073bd89ecd35f97dc851127c1
SKane <le_cas315@hotmail.com>
2020-02-27 17:47:07 +1000

    cv.py beta update

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -1,8 +1,15 @@
 @client.command(description='Creates a vote')
 @commands.has_role('Owner')
 async def cv(ctx, timer, *, desc):
-    embed = discord.Embed(title=f'Vote [Timer: {timer}]', color=discord.Color.from_rgb(0, 255, 0), description=desc)
-    embed.set_footer(text=f'Author: {ctx.message.author}')
+    global vote_amount
+    embed = discord.Embed(title=f'#{vote_amount} Vote [{timer}min]', color=discord.Color.from_rgb(0, 255, 0), description=desc)
+    embed.set_footer(text=f'Vote started by: {ctx.message.author}')
     vote = await ctx.channel.send(embed=embed)
-    for i in [':white_check_mark:', ':no_entry_sign:']:
+
+    for i in ['✅', '⛔']:
         await vote.add_reaction(i)
+    await asyncio.sleep(timer * 60)
+
+    embed = discord.Embed(title=f'Vote #{vote_amount}', color=discord.Color.from_rgb(0, 255, 0), description='Vote ended!')
+    embed.set_footer(text=f'Ended the vote by: {ctx.message.author}')
+    await ctx.channel.send(embed=embed)
