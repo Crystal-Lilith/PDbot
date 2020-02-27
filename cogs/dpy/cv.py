3a16546f66285f473faf7f147b8a276ce3afc0af
SKane <le_cas315@hotmail.com>
2020-02-27 20:25:14 +1000

    cv.py beta v0.0.3

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -28,9 +28,9 @@ async def cv(ctx, timer, *, desc):
 
     for i in vote.reactions:
         if i.emoji == '✅':
-            yes = vote.reactions.count(i.emoji)
+            yes = vote.reactions.count(i)
         elif i.emoji == '🚫':
-            no = vote.reactions.count(i.emoji)
+            no = vote.reactions.count(i)
         else:
             await ctx.channel.send('not working')
     embed = discord.Embed(title=f'Vote #{vote_amount}', color=discord.Color.from_rgb(0, 255, 0), description=f'Vote ended!\n✅: {yes}\n🚫: {no}')
