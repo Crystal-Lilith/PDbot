c7b717781c8d4584b5ba6ca58fe48ce08a4141c5
SKane <le_cas315@hotmail.com>
2020-02-27 17:51:41 +1000

    cv.py update

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -5,7 +5,7 @@ async def cv(ctx, timer, *, desc):
     if 'vote_amount' in globals():
         pass
     else:
-        vote_amount = 0
+        vote_amount = 1
     embed = discord.Embed(title=f'#{vote_amount} Vote [{timer}min]', color=discord.Color.from_rgb(0, 255, 0), description=desc)
     embed.set_footer(text=f'Vote started by: {ctx.message.author}')
     vote = await ctx.channel.send(embed=embed)
@@ -14,7 +14,7 @@ async def cv(ctx, timer, *, desc):
 
     for i in ['✅', '⛔']:
         await vote.add_reaction(i)
-    await asyncio.sleep(timer * 60)
+    await asyncio.sleep(int(timer) * 60)
 
     embed = discord.Embed(title=f'Vote #{vote_amount}', color=discord.Color.from_rgb(0, 255, 0), description='Vote ended!')
     embed.set_footer(text=f'Ended the vote by: {ctx.message.author}')
