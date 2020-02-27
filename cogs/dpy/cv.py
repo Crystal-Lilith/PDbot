cda6e5a361a94fec0c2607a587ea7cbbd6faa02c
SKane <le_cas315@hotmail.com>
2020-02-27 18:46:59 +1000

    cv.py fetch fix

--- cogs/dpy/cv.py
+++ cogs/dpy/cv.py
@@ -23,7 +23,7 @@ async def cv(ctx, timer, *, desc):
 
     for i in ['✅', '⛔']:
         await vote.add_reaction(i)
-    vote = get(ctx.channel.fetch_message, id=vote.id)
+    vote = ctx.channel.fetch_message(vote.id)
     await asyncio.sleep(float(timer) * 60)
 
     await ctx.channel.send(vote.reactions)
