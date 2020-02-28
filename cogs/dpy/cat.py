bd9f93558bd66f300a6282550622c3e314572e43
SKane <le_cas315@hotmail.com>
2020-02-28 19:07:39 +1000

    cat.py security patch

--- cogs/dpy/cat.py
+++ cogs/dpy/cat.py
@@ -13,17 +13,18 @@ async def cat(ctx, *, directory):
                                 description='You may not cat this file!')
                 embed.set_footer(text=f'Attempted by: {ctx.message.author}')
                 await ctx.channel.send(embed=embed)
-            with open(directory, 'r') as f:
-                lang = ''
-                if i.endswith('.py'):
-                    lang = 'python'
-                elif i.endswith('.rb'):
-                    lang = 'ruby'
-                else:
-                    pass
-                embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```{lang}\n{f.read()}```')
-                embed.set_footer(text=f'Requested by: {ctx.message.author}')
-                await ctx.channel.send(embed=embed)
+            else:
+                with open(directory, 'r') as f:
+                    lang = ''
+                    if i.endswith('.py'):
+                        lang = 'python'
+                    elif i.endswith('.rb'):
+                        lang = 'ruby'
+                    else:
+                        pass
+                    embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```{lang}\n{f.read()}```')
+                    embed.set_footer(text=f'Requested by: {ctx.message.author}')
+                    await ctx.channel.send(embed=embed)
     except:
         embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
                                 description='File doesn\'t exist!')
