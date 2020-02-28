31625f47fe0ab6cc3bc25af4c31030325d435ea7
SKane <le_cas315@hotmail.com>
2020-02-28 18:21:55 +1000

    ls.py fix

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -4,15 +4,16 @@ async def ls(ctx, *, directory='.'):
     try:
         x = ''
         for i in os.listdir(directory):
-            if x.lower() not in [".env", "start.sh"]:
+            if i.lower() not in [".env", "start.sh"]:
                 x = f"{x}{i}\n"
-                embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
-                embed.set_footer(text=f'Requested by: {ctx.message.author}')
             else:
                 embed = discord.Embed(title='Warning ❗', color=discord.Color.from_rgb(178, 34, 34),
                                 description='You may not ls this directory!')
                 embed.set_footer(text=f'Attempted by: {ctx.message.author}')
-            await ctx.channel.send(embed=embed)
+                break
+        embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
+        embed.set_footer(text=f'Requested by: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
     except:
         embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
                                 description='Directory doesn\'t exist!')
