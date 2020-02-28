7167d398510a3225df6c82c43fcf70affdb823be
SKane <le_cas315@hotmail.com>
2020-02-28 16:10:54 +1000

    cat.py & ls.py security fix

--- cogs/dpy/cat.py
+++ cogs/dpy/cat.py
@@ -2,9 +2,16 @@
 @commands.has_any_role('PDBot Mod', 'PDBot Dev')
 async def cat(ctx, *, directory):
     try:
+        for i in directory.split('/'):
+            if i.lower() in [".env", "start.sh"]:
+                embed = discord.Embed(title='Warning! ❗', color=discord.Color.from_rgb(178, 34, 34),
+                                description='You may not cat this file!')
+                embed.set_footer(text=f'Attempted by: {ctx.message.author}')
+                return
+        await ctx.channel.send(embed=embed)
         with open(directory, 'r') as f:
             embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{f.read()}```')
-            embed.set_footer(text=f'Attempted by: {ctx.message.author}')
+            embed.set_footer(text=f'Requested by: {ctx.message.author}')
             await ctx.channel.send(embed=embed)
     except:
         embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
