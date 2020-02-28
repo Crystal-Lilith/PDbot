952dd431a1baa8e4487b55ce11cf56bdaec4a341
Proxy <proxneon960@hotmail.com>
2020-02-28 08:37:35 +0000

    Update 'cogs/dpy/ls.py'

--- cogs/dpy/ls.py
+++ cogs/dpy/ls.py
@@ -1,16 +1,11 @@
 @client.command(description='Shows you all files in the specified directory')
 @commands.has_any_role('PDBot Mod', 'PDBot Dev')
-async def ls(ctx, *, directory='.'):
+async def ls(ctx, *, directory=None):
     try:
         x = ''
         for i in os.listdir(directory):
             if i.lower() not in [".env", "start.sh"]:
                 x = f"{x}{i}\n"
-            else:
-                embed = discord.Embed(title='Warning ❗', color=discord.Color.from_rgb(178, 34, 34),
-                                description='You may not ls this directory!')
-                embed.set_footer(text=f'Attempted by: {ctx.message.author}')
-                break
         embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```css\n{x}```')
         embed.set_footer(text=f'Requested by: {ctx.message.author}')
         await ctx.channel.send(embed=embed)
