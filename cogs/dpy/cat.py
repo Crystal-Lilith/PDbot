dbc120e64b9c2b6b47a8b38562d376c400f8e996
Proxy <proxneon960@hotmail.com>
2020-02-28 08:48:11 +0000

    Update 'cogs/dpy/cat.py'

--- cogs/dpy/cat.py
+++ cogs/dpy/cat.py
@@ -1,27 +1,29 @@
 @client.command(description='Outputs file content')
 @commands.has_any_role('PDBot Mod', 'PDBot Dev')
 async def cat(ctx, *, directory):
+    if directory.startswith("./"):
+        pass
+    else:
+        directory = f"./{directory}"
     try:
         split_directory = directory.split('/')
         for i in split_directory:
-            if i.lower() in [".env", "start.sh"]:
+            if i.lower() in ["..", ".env", "start.sh"]:
                 embed = discord.Embed(title='Warning ❗', color=discord.Color.from_rgb(178, 34, 34),
                                 description='You may not cat this file!')
                 embed.set_footer(text=f'Attempted by: {ctx.message.author}')
                 await ctx.channel.send(embed=embed)
-            elif i == split_directory[-1]:
-                with open(directory, 'r') as f:
-                    i = i.lower().split('.')
-                    lang = ''
-                    if i[-1] == 'py':
-                        lang = 'python'
-                    elif i[-1] == 'rb':
-                        lang = 'ruby'
-                    else:
-                        pass
-                    embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```{lang}\n{f.read()}```')
-                    embed.set_footer(text=f'Requested by: {ctx.message.author}')
-                    await ctx.channel.send(embed=embed)
+            with open(directory, 'r') as f:
+                lang = ''
+                if i.endswith('.py'):
+                    lang = 'python'
+                elif i.endswith('.rb'):
+                    lang = 'ruby'
+                else:
+                    pass
+                embed = discord.Embed(title=f'List of files and folders in `{directory}`', color=discord.Color.from_rgb(0, 191, 255), description=f'```{lang}\n{f.read()}```')
+                embed.set_footer(text=f'Requested by: {ctx.message.author}')
+                await ctx.channel.send(embed=embed)
     except:
         embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
                                 description='File doesn\'t exist!')
