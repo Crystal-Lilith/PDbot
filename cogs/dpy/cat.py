b5148488e805b14c6ffde70f03331d58fdb52645
SKane <le_cas315@hotmail.com>
2020-02-28 18:30:47 +1000

    cat.py fix

--- cogs/dpy/cat.py
+++ cogs/dpy/cat.py
@@ -2,14 +2,14 @@
 @commands.has_any_role('PDBot Mod', 'PDBot Dev')
 async def cat(ctx, *, directory):
     try:
-        directory = directory.split('/')
-        for i in directory:
+        split_directory = directory.split('/')
+        for i in split_directory:
             if i.lower() in [".env", "start.sh"]:
                 embed = discord.Embed(title='Warning ❗', color=discord.Color.from_rgb(178, 34, 34),
                                 description='You may not cat this file!')
                 embed.set_footer(text=f'Attempted by: {ctx.message.author}')
                 await ctx.channel.send(embed=embed)
-            elif i == directory[-1]:
+            elif i == split_directory[-1]:
                 with open(directory, 'r') as f:
                     i = i.lower().split('.')
                     lang = ''
