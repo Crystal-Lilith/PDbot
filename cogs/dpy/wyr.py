0dc31ebbb6d09f334811434cbf91c6e08ffa0459
SKane <le_cas315@hotmail.com>
2020-02-29 17:21:58 +1000

    wyr.py beta

--- cogs/dpy/wyr.py
+++ cogs/dpy/wyr.py
@@ -1,17 +1,17 @@
 @client.command(description='Gives a would you rather question||$wr')
 async def wyr(ctx):
-    # try:
-    page = requests.get('https://www.signupgenius.com/groups/would-you-rather.cfm')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    questions = soup.find('ol').find_all('li')
-    #question = questions.get_text() # random.choice(questions).
+    try:
+        page = requests.get('https://conversationstartersworld.com/would-you-rather-questions/')
+        soup = BeautifulSoup(page.content, 'html.parser')
+        questions = soup.find(class_='post-456 page type-page status-publish has-post-thumbnail entry').find_all('h3')
+        question = random.choice(questions).get_text()[5:]
 
-    embed = discord.Embed(title='Would you rather?', color=discord.Color.from_rgb(0, 191, 255), 
-                            description=questions)
-    embed.set_footer(text=f'Requested by: {ctx.message.author}')
-    await ctx.channel.send(embed=embed)
-    # except:
-        # embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-        #                         description='Failed to get questions')
-        # embed.set_footer(text=f'Attempted by: {ctx.message.author}')
-        # await ctx.channel.send(embed=embed)
+        embed = discord.Embed(title='Would you rather?', color=discord.Color.from_rgb(0, 191, 255), 
+                                description=question)
+        embed.set_footer(text=f'Requested by: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
+    except:
+        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
+                                description='Failed to get questions')
+        embed.set_footer(text=f'Attempted by: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
