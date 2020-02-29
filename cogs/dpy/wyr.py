2e0a2f6fcc197b092d4b9fbcc6fa2ccd4de54bef
SKane <le_cas315@hotmail.com>
2020-02-29 16:41:49 +1000

    wdr.py beta

---
+++ cogs/dpy/wyr.py
@@ -0,0 +1,17 @@
+@client.command(description='Gives a would you rather question||$wr')
+async def wyr(ctx):
+    try:
+        page = requests.get('https://www.signupgenius.com/groups/would-you-rather.cfm')
+        soup = BeautifulSoup(page.content, 'html.parser')
+        questions = soup.find('li')
+        question = random.choice(questions)
+
+        embed = discord.Embed(title='Would you rather?', color=discord.Color.from_rgb(0, 191, 255), 
+                                description=question)
+        embed.set_footer(text=f'Requested by: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
+    except:
+        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
+                                description='Failed to get questions')
+        embed.set_footer(text=f'Attempted by: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
