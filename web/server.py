a085174c7f79f4fb8f8244aa4952059712528f3e
IpProxyNeon <proxneon960@hotmail.com>
2020-02-29 13:11:27 +0000

    Added the webserver

--- web/server.py
+++ web/server.py
@@ -1,3 +1,10 @@
+try:
+    import flask
+    del(flask)
+except:
+    import os
+    os.system("pip3 install flask")
+
 from flask import Flask
 
 app=Flask(__name__)
@@ -7,9 +14,9 @@ def index():
     with open("./index.html") as f:
         return f.read()
 
-@app.route("/<path>")
-def page_loader(path):
-    with open("./"+path) as f:
+@app.route("/<path:filepath>")
+def page_loader(filepath):
+    with open("./"+filepath) as f:
         return f.read()
 
 app.run("0.0.0.0", 9000)
