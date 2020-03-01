d973dc62b6473447737cc2bdc852c1cc6337efb8
SKane <le_cas315@hotmail.com>
2020-03-02 00:59:54 +1000

    asciiart debug

--- cogs/dpy/asciiart.py
+++ cogs/dpy/asciiart.py
@@ -1,12 +1,12 @@
 @client.command(description='Creates ascii art||<font> <message>')
 async def asciiart(ctx, font, *, message):
     try:
-        embed = discord.Embed(color=discord.Color.from_rgb(178, 34, 34),
-                                description=pyfiglet.figlet_format(message, font=font))
-        embed.set_footer(text=f'Requested by: {ctx.message.author}')
-        await ctx.channel.send(embed=embed)
+        # embed = discord.Embed(color=discord.Color.from_rgb(178, 34, 34),
+        #                         description=pyfiglet.figlet_format(message, font=font))
+        # embed.set_footer(text=f'Requested by: {ctx.message.author}')
+        await ctx.channel.send(pyfiglet.figlet_format(message, font=font))
     except:
         embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-                                description='Failed!')
+                                description='Font doesn\'t exist!')
         embed.set_footer(text=f'Attempted by: {ctx.message.author}')
         await ctx.channel.send(embed=embed)
