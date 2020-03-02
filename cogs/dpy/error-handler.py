88076401510849082c23368156910dd2e556446f
4-way mirror <proxneon017@gmail.com>
2020-03-02 02:52:11 +0000

    Codeberg mirror

--- cogs/dpy/error-handler.py
+++
@@ -1,7 +0,0 @@
-@client.listen()
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        pass
-    else:
-        print(error)
-        await ctx.channel.send(error)
--- stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-clear && killall ruby python3
