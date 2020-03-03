f4e820640a02738b72550e98d6dcfdecbce4e3fc
4-way mirror <proxneon017@gmail.com>
2020-03-03 22:54:20 +0000

    GitHub mirror

---
+++ pdaddons/python/hata/ext.py
@@ -0,0 +1,40 @@
+from hata import Embed
+
+def HelpPages(client, message)
+    cmds = []
+    for i in client.events.message_create.commands:
+        cmds.append(i)
+    for y in listdir('./cmds/'):
+        with open('./cmds/'+y,) as f:
+            x = json.load(f)
+        for i in x:
+            if i.lower() != 'help':
+                cmds.append(i)
+    cmds = sorted(cmds, key=str.lower) # Key positional argument is used to make sure uppercase commands don't take precedence.
+    pages=[]
+    part=[]
+    index=0
+    for element in cmds:
+        if index==16:
+            pages.append('\n'.join(part))
+            part.clear()
+            index=0
+        part.append(f'**>>** {element}')
+        index+=1
+
+    pages.append('\n'.join(part))
+
+    del part
+
+    result=[]
+
+    limit=len(pages)
+    index=0
+    while index<limit:
+        embed=Embed('Commands:',color='029320',description=pages[index])
+        index+=1
+        embed.add_field("Do $meme for some funny memes!", f'page {index}/{limit}')
+        result.append(embed)
+
+    del pages
+    return result
