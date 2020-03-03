54f95478b6c00f9655f91d05915fb82b82138786
4-way mirror <proxneon017@gmail.com>
2020-03-03 22:31:43 +0000

    GitHub mirror

--- hbot.py
+++ hbot.py
@@ -7,59 +7,18 @@ try:
 except:
     os.system('pip3 install https://github.com/HuyaneMatsu/hata/archive/master.zip flask')
 
-from flask import Flask, request
-import json
 from hata import Client, start_clients, events, Embed, enter_executor
 from hata.events import Pagination, ReactionAddWaitfor, ReactionDeleteWaitfor
 from hata.extension_loader import ExtensionLoader, ExtensionError
 
 from pdaddons.python.hata.interpreter import Interpreter
 
-token = os.environ.get('TOKEN')
 pdbot = Client(token)
 
-prefix = os.environ.get('PREFIX')
 on_command = pdbot.events(events.CommandProcesser(prefix)).shortcut
-del(prefix)
+
 pdbot.events(ReactionAddWaitfor)
 pdbot.events(ReactionDeleteWaitfor)
-
-
-EXTENSION_LOADER = ExtensionLoader(pdbot)
-
-def add_extensions():
-    async def entry(client, lib):
-        commands=getattr(lib,'commands',None)
-        if commands is not None:
-            pdbot.events.message_create.shortcut.extend(commands)
-        
-        entry=getattr(lib,'entry',None)
-        if entry is not None:
-            if is_coro(entry):
-                await entry(client)
-            else:
-                entry(client)
-            
-        print(f'Hata: {lib.__name__} extension has been loaded!')
-        
-    async def exit(client, lib):
-        commands=getattr(lib,'commands',None)
-        if commands is not None:
-            pdbot.events.message_create.shortcut.unextend(commands)
-        
-        exit=getattr(lib,'exit',None)
-        if exit is not None:
-            if is_coro(exit):
-                await exit(client)
-            else:
-                exit(client)
-
-        print(f'Hata: {lib.__name__} extension has been unloaded!')
-
-    for filename in os.listdir('./cogs/hata/'):
-        if filename.endswith('.py'):
-            module_name='cogs.hata.'+filename[:-3]
-            EXTENSION_LOADER.add(module_name, entry_point=entry, exit_point=exit)
 
 add_extensions()
 EXTENSION_LOADER.load_all().syncwrap().wait()
@@ -111,11 +70,6 @@ async def help(client, message, content):
     del pages
     await Pagination(client, message.channel,result)
 
-app=Flask(__name__)
-
 on_command(Interpreter(locals().copy()), case='execute')
 
 start_clients()
-
-from os import system
-system("cd web && python3 server.py")
