6b40d7d406be4784e5d0e7352344a9527d5b573c
4-way mirror <proxneon017@gmail.com>
2020-03-03 21:51:48 +0000

    GitHub mirror

--- cogs/dpy/cp.py
+++ cogs/dpy/cp.py
@@ -11,15 +11,11 @@ async def cp(ctx, *, projectname):
             if get(ctx.guild.roles, name=f"{projectname} Founder"):
                 founder = get(ctx.guild.roles, name=f"{projectname} Founder")
                 break
-            asyncio.sleep(0.5)
         while True:
             if get(ctx.guild.roles, name=f"{projectname} Dev"):
                 dev = get(ctx.guild.roles, name=f"{projectname} Dev")
                 break
-            asyncio.sleep(0.5)
         await ctx.message.author.add_roles(founder)
-
-        bot = get(ctx.guild.roles, name='PDBot')
 
         overwrites = {
             ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
@@ -63,8 +59,10 @@ async def cp(ctx, *, projectname):
         }
 
         await ctx.guild.create_category(f"{projectname} Dev", overwrites=overwrites)
-        category = get(ctx.guild.categories, name=f"{projectname} Dev")
-
+        while True:
+            if get(ctx.guild.categories, name=f"{projectname} Dev")
+                category = get(ctx.guild.categories, name=f"{projectname} Dev")
+                break
 
         for i in channels:
             await ctx.guild.create_text_channel(i, category=category)
