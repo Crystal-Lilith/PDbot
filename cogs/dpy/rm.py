41e9fe70ad2c114c50671821c8479606eaca81e9
Proxy <proxneon960@hotmail.com>
2020-03-04 22:11:59 +0000

    Update 'cogs/dpy/rm.py'

--- cogs/dpy/rm.py
+++ cogs/dpy/rm.py
@@ -1,6 +1,10 @@
 @client.command(description='Deletes messages in a channel||<amount>')
-@commands.has_role('Owner')
 async def rm(ctx, amount):
+    if not ctx.author.permissions_in(ctx.channel).manage_messages:
+        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51), description='You do not have the correct permissions (manage messages) to use this command!')
+        embed.set_footer(text=f'Attempted By: {ctx.message.author}')
+        await ctx.channel.send(embed=embed)
+        return
     try:
         amount = int(amount)
         embed = discord.Embed(color=discord.Color.from_rgb(0, 255, 0), description=f'Deleted {amount} messages!')
