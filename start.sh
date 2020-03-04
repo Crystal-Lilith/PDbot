4109cb5daaf3986c3cee8619b65e1ba74f602ea2
Proxy <proxneon960@hotmail.com>
2020-03-04 21:01:24 +0000

    Update 'start.sh'

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ export PREFIX
 gem install discordrb dotenv
 while :
 do
-    ruby rbot.rb && python3 web/server.py && python3 
+    ruby rbot.rb & python3 web/server.py & python3 dbot.py & python3 hbot.py
     killall python3 ruby
     git fetch
     git pull
