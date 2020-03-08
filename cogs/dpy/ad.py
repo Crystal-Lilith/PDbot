50f64ad562ee644fd16ae737d3d20907f1958e65
SKane <le_cas315@hotmail.com>
2020-03-08 23:31:09 +1000

    error-handling fix

--- cogs/dpy/ad.py
+++ cogs/dpy/ad.py
@@ -46,9 +46,8 @@ async def ad_error(ctx, error):
         message = await ctx.channel.fetch_message(ctx.message.id)
         user_cmd = message.content.split()[0].split('$')
         cmd = get(client.commands, name=user_cmd[1])
-        desc = user_cmd[1]
         syntax = cmd.description.split('||')[1]
         embed = discord.Embed(title='Invalid Syntax! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-                                description=f'{desc} ({syntax})')
+                                description=f'{user_cmd} ({syntax})')
         embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
         await ctx.channel.send(embed=embed)
