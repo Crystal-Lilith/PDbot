5afc1993dd555d813a68459469364d6328669ca5
SKane <le_cas315@hotmail.com>
2020-03-08 21:44:55 +1000

    error-handler update

--- cogs/dpy/error-handler.py
+++ cogs/dpy/error-handler.py
@@ -2,16 +2,15 @@
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         pass
-    elif isinstance(error, commands.BadArgument):
+    elif isinstance(error, commands.MissingRequiredArgument):
         message = await ctx.channel.fetch_message(ctx.message.id)
         user_cmd = message.content.split()[0]
         desc = user_cmd.description.split('||')[0]
         syntax = user_cmd.description.split('||')[0]
-        await ctx.channel.send(f'{desc} ({syntax})')
-        # embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-        #                         description=f'{desc} ({syntax})')
-        # embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-        # await ctx.channel.send(embed=embed)
+        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
+                                description=f'{desc} ({syntax})')
+        embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+        await ctx.channel.send(embed=embed)
     else:
         print(error)
         await ctx.channel.send(error)
