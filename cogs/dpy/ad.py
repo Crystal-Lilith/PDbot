ae33e0bf0eb7db39890baf061d10e2866e5a01f9
SKane <le_cas315@hotmail.com>
2020-03-08 22:17:12 +1000

    error-handling patch

--- cogs/dpy/ad.py
+++ cogs/dpy/ad.py
@@ -44,9 +44,9 @@ async def ad(ctx, user: discord.User, *, projectname):
 async def ad_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         message = await ctx.channel.fetch_message(ctx.message.id)
-        user_cmd = message.content.split()
-        desc = user_cmd.split('||')[0]
-        syntax = user_cmd.split('||')[1]
+        user_cmd = message.content.split('||')
+        desc = user_cmd[0]
+        syntax = user_cmd[1]
         embed = discord.Embed(title='Invalid Syntax! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
                                 description=f'{desc} ({syntax})')
         embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
