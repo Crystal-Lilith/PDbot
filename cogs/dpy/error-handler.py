ad31a29be20b08f9060c2f2f4b6a375e212a47eb
SKane <le_cas315@hotmail.com>
2020-03-08 21:50:28 +1000

    error-handler fix

--- cogs/dpy/error-handler.py
+++ cogs/dpy/error-handler.py
@@ -1,8 +1,8 @@
 @client.listen()
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
+    if isinstance(error, ctx.commands.CommandNotFound):
         pass
-    elif isinstance(error, commands.MissingRequiredArgument):
+    elif isinstance(error, ctx.commands.MissingRequiredArgument):
         message = await ctx.channel.fetch_message(ctx.message.id)
         user_cmd = message.content.split()[0]
         desc = user_cmd.description.split('||')[0]
