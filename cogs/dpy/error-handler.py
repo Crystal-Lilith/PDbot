d352866d1fc1120cc6cf197ab90462301245b69c
SKane <le_cas315@hotmail.com>
2020-03-08 21:33:57 +1000

    error-handler debug

--- cogs/dpy/error-handler.py
+++ cogs/dpy/error-handler.py
@@ -7,10 +7,11 @@ async def on_command_error(ctx, error):
         user_cmd = message.content.split()[0]
         desc = user_cmd.description.split('||')[0]
         syntax = user_cmd.description.split('||')[0]
-        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-                                description=f'{desc} ({syntax})')
-        embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-        await ctx.channel.send(embed=embed)
+        await ctx.channel.send(f'{desc} ({syntax})')
+        # embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
+        #                         description=f'{desc} ({syntax})')
+        # embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+        # await ctx.channel.send(embed=embed)
     else:
         print(error)
         await ctx.channel.send(error)
