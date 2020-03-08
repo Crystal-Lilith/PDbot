fa272b236521c18faef415a45394507087760323
SKane <le_cas315@hotmail.com>
2020-03-08 22:02:41 +1000

    syntax check beta

--- cogs/dpy/error-handler.py
+++ cogs/dpy/error-handler.py
@@ -1,14 +1,8 @@
 @client.listen()
 async def on_command_error(ctx, error):
-    if isinstance(error, discord.commands.CommandNotFound):
-        pass
-    elif isinstance(error, discord.commands.MissingRequiredArgument):
-        message = await ctx.channel.fetch_message(ctx.message.id)
-        user_cmd = message.content.split()[0]
-        desc = user_cmd.description.split('||')[0]
-        syntax = user_cmd.description.split('||')[0]
-        embed = discord.Embed(title='Error! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
-                                description=f'{desc} ({syntax})')
+    if isinstance(error, commands.CommandNotFound):
+        embed = discord.Embed(title='Unknown Command! ⚠️', color=discord.Color.from_rgb(255, 255, 51),
+                                description='Command not found! Do `$help` for a list of commands!')
         embed.set_footer(text=f'Attempted by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
         await ctx.channel.send(embed=embed)
     else:
