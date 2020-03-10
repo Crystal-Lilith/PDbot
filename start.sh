f18776cf38bbead2fed18790f21f360a22f3c2ee
SKane <le_cas315@hotmail.com>
2020-03-10 21:36:28 +1000

    start.sh export fix

--- start.sh
+++ start.sh
@@ -2,9 +2,12 @@
 clear
 if [ -f .env ]
 then
-  export $(cut -d= -f1 .env)
+  export $(cat .env | sed 's/#.*//g' | xargs)
 fi
-
+export TOKEN
+export PREFIX
+export CLIENT_ID
+export CLIENT_SECRET
 sudo gem install discordrb dotenv
 while :
 do
