9a3f6f4dc4c5431aa287aea96d6bc65945a776e4
Ubuntu <ubuntu@noreply.codeberg.org>
2020-03-12 16:45:28 +0100

    Update 'cogs/dpy/dm.py'

--- cogs/dpy/dm.py
+++ cogs/dpy/dm.py
@@ -1,3 +1,4 @@
-@client.command()
-async def dm(ctx, user: discord.User)
-        await user.send('This Message is send in DM')
+@Commands.has_roles(673405620527038478,682105898642047233)
+async def dm(ctx, user: discord.User, msg):
+  await user.send(msg)
+  ctx.channel.send('Message Sent')
