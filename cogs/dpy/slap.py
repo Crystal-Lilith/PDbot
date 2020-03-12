1d6ae8a24a3d517b9e18fb89ff8c0accdfa5b2c6
Ubuntu <ubuntu@noreply.codeberg.org>
2020-03-12 15:50:21 +0100

    Update 'cogs/dpy/slap.py'

--- cogs/dpy/slap.py
+++ cogs/dpy/slap.py
@@ -1,9 +1,3 @@
 @client.command()
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$slap'):
-        msg = f”*Slapped* {ctx.author.mention}”
-        await ctx.channel.send(msg)
+async def slap(ctx):
+  await ctx.channel.send(f”*Slapped {ctx.author.mention}”)
