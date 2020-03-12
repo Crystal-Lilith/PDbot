ccdb1a61124a5eec1c6ee8f09037f4695374a756
SKane <le_cas315@hotmail.com>
2020-03-12 16:55:26 +1000

    audit-log debug

--- cogs/dpy/audit-log.py
+++ cogs/dpy/audit-log.py
@@ -1,8 +1,8 @@
 @client.listen()
-async def on_message_delete(message):
+async def on_raw_message_delete(message):
     log_channel = get(message.guild.channels, name='log')
     # embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34),
     #                             description=message.content)
     # embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
     # await log_channel.send(embed=embed, files=message.attachments)
-    await message.send(message.content)
+    print(message.content)
