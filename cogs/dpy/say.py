f4fa8e358b58e4503add5ff699ffb2526ad0b828
SKane <le_cas315@hotmail.com>
2020-03-13 02:43:01 +1000

    say.py patch

--- cogs/dpy/say.py
+++ cogs/dpy/say.py
@@ -1,4 +1,5 @@
 @Client.command()
+@commands.has_any_role('PDBot Dev', 'PDBot Mod')
 async def dm(ctx, category, channel_name, *, msg):
   category = get(ctx.guild.categories, name=f"{projectname} Dev")
   msg_channel = get(ctx.guild.channel, category=category, name=channel_name)
