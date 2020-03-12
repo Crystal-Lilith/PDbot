4ce024a5efebcaf086fd284ee048c4b511ef0b20
SKane <le_cas315@hotmail.com>
2020-03-13 02:04:12 +1000

    dm.py fix

--- cogs/dpy/dm.py
+++ cogs/dpy/dm.py
@@ -1,5 +1,5 @@
 @client.command()
-@commands.has_roles(673405620527038478,682105898642047233)
+@commands.has_any_role(673405620527038478,682105898642047233)
 async def dm(ctx, user: discord.User, msg):
   await user.send(msg)
   ctx.channel.send('Message Sent')
