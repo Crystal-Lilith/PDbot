b3631237703efafa292f9bcd9b8d139ca61ee33a
SKane <le_cas315@hotmail.com>
2020-03-12 20:26:55 +1000

    audit-log.py fix

--- cogs/dpy/audit-log.py
+++ cogs/dpy/audit-log.py
@@ -1,13 +1,13 @@
 @client.listen()
 async def on_message_delete(message):
     log_channel = get(message.guild.channels, name='log')
-    if message.content:
+    try:
         embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34))
         embed.add_field(name='Content:', value=message.content)
-        embed.add_field(name='Channel:', value=message.channel.mention)
-        embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
-    else:
+    except:
         embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34),
                                 description='Unable to recover')
 
+    embed.add_field(name='Channel:', value=message.channel.mention)
+    embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
     await log_channel.send(embed=embed)
