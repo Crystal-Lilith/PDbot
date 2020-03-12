fc35272b9a74f1467937e6a831a3851ffeebb34d
SKane <le_cas315@hotmail.com>
2020-03-13 02:46:25 +1000

    say.py removal

--- cogs/dpy/say.py
+++
@@ -1,6 +0,0 @@
-@Client.command()
-@commands.has_any_role('PDBot Dev', 'PDBot Mod')
-async def dm(ctx, category, channel_name, *, msg):
-  category = get(ctx.guild.categories, name=f"{projectname} Dev")
-  msg_channel = get(ctx.guild.channel, category=category, name=channel_name)
-  await msg_channel.send(msg)
