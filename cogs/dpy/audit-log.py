0646bb9212e5c2ff5de3a0602e45a0662a8abf8a
SKane <le_cas315@hotmail.com>
2020-03-12 20:07:27 +1000

    audit-log.py update

--- cogs/dpy/audit-log.py
+++ cogs/dpy/audit-log.py
@@ -8,5 +8,5 @@ async def on_message_delete(message):
         embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34),
                                 description='Unable to recover')
 
-    embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
+    embed.set_footer(text=f'Deleted by: {message.author} | In #{message.channel}', icon_url=message.author.avatar_url)
     await log_channel.send(embed=embed)
