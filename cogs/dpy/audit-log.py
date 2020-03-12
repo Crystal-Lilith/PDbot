6a76713df90a37220bf5847ecc541a2b8b951a73
SKane <le_cas315@hotmail.com>
2020-03-12 20:06:12 +1000

    Revert "audit-log.py test"
    This reverts commit 0ce2210ef2793757cbbad26774d1ab48cb873167.

--- cogs/dpy/audit-log.py
+++ cogs/dpy/audit-log.py
@@ -3,11 +3,10 @@ async def on_message_delete(message):
     log_channel = get(message.guild.channels, name='log')
     if message.content:
         embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34),
-                                description=message.content)
-        embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
-        await log_channel.send(embed=embed, files=message.attachments)
+                                description=message.content)        
     else:
         embed = discord.Embed(title='Message Deleted 🗑️', color=discord.Color.from_rgb(178, 34, 34),
                                 description='Unable to recover')
-        embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
-        await log_channel.send(embed=embed)
+
+    embed.set_footer(text=f'Deleted by: {message.author}', icon_url=message.author.avatar_url)
+    await log_channel.send(embed=embed)
