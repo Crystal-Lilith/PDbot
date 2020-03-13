dd4726970ab78efc6839e9f7db931a8964ef7b44
Ubuntu <ubuntu@noreply.codeberg.org>
2020-03-13 18:02:53 +0100

    Add 'cogs/dpy/ddos.py'

---
+++ cogs/dpy/ddos.py
@@ -0,0 +1,8 @@
+@client.command()
+async def ddos(ctx, user: discord.User)
+await ctx.channel.send(f'DDOSing {user.name})
+    variable = [
+        "DDOS Complete!",
+        "DDOS Failed :(",]
+    await bot.say(ctx.message.channel, "{}".format(random.choice(variable)))
+                       await ctx.channel.send('{author.mention}, Bruh, Did You For Get I Cant DDOS?)
