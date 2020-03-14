e1b4d8d9021be59dc05ccdb6db768537a63fde1e
SKane <le_cas315@hotmail.com>
2020-03-14 11:45:32 +1000

    ddos.py fix

--- cogs/dpy/ddos.py
+++ cogs/dpy/ddos.py
@@ -1,6 +1,8 @@
+
+
 @client.command(description='Fake DDOS||<username>')
 async def ddos(ctx, user: discord.User):
     await ctx.channel.send(f'DDOSing {user.name}')
     status = ['DDOS Complete!', 'DDOS Failed']
-    await ctx.channel.send(random.choice(variable))
+    await ctx.channel.send(random.choice(status))
     await ctx.channel.send(f"{author.mention}, Bruh, did you forget that I can't DDOS?")
