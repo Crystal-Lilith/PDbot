1db3e655ce51a6086a5b9f047166c3932d8f7455
SKane <le_cas315@hotmail.com>
2020-03-15 02:04:37 +1000

    email.js fix

--- web/static/src/js/contact/email.js
+++ web/static/src/js/contact/email.js
@@ -1,21 +1,16 @@
 function email_copy() {
-    /* Get the text field */
-    const copyText = document.querySelector('#email');
-
-    copyText.addEventListener('click', () => {      
-        const el = document.createElement('textarea');
-        el.value = 'skane@pden.net';
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-        Swal.fire({
-            icon: 'success',
-            title: 'Copied to clipboard!',
-            background: '#1d1e22',
-            padding: '6rem',
-            showConfirmButton: false,
-            timer: 1500
-        })
+    const el = document.createElement('textarea');
+    el.value = 'skane@pden.net';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    Swal.fire({
+        icon: 'success',
+        title: 'Copied to clipboard!',
+        background: '#1d1e22',
+        padding: '6rem',
+        showConfirmButton: false,
+        timer: 1500
     })
 }
