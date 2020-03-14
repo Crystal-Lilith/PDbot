6ed5d4abfbc7c6fb967c343fc0c41b387cd367d7
SKane <le_cas315@hotmail.com>
2020-03-14 22:43:28 +1000

    email.js temp debug

--- web/static/src/js/contact/email.js
+++ web/static/src/js/contact/email.js
@@ -2,13 +2,13 @@ function email_copy() {
     const copyText = document.getElementById('email');
 
     copyText.addEventListener('click', () => {
-        /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+        // /* Select the text field */
+        // copyText.select();
+        // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
 
-        /* Copy the text inside the text field */
-        document.execCommand("copy");
-        /* Alert the copied text */
+        // /* Copy the text inside the text field */
+        // document.execCommand("copy");
+        // /* Alert the copied text */
         Swal.fire({
             position: 'top-start',
             icon: 'success',
