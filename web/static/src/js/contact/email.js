b83de8e8c2458e35d659f364a2a6cdbe97c3f774
SKane <le_cas315@hotmail.com>
2020-03-14 14:04:05 +1000

    contact page update

---
+++ web/static/src/js/contact/email.js
@@ -0,0 +1,22 @@
+function email_copy() {
+    /* Get the text field */
+    var copyText = document.getElementById("cta");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    copyText.addEventListener('click', () => {
+        Swal.fire({
+            position: 'top-start',
+            icon: 'success',
+            title: 'Copied to clipboard!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    })
+}
