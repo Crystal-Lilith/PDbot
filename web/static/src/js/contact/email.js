0d4dd77f49b2c7b734dda855a8f20baf3ebea1f6
SKane <le_cas315@hotmail.com>
2020-03-14 14:12:49 +1000

    contact.html fix

--- web/static/src/js/contact/email.js
+++ web/static/src/js/contact/email.js
@@ -1,16 +1,15 @@
 const email_copy = () => {
     /* Get the text field */
-    var copyText = document.getElementById("cta");
+    var copyText = document.getElementById("email");
 
     /* Select the text field */
     copyText.select();
     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
 
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-
     /* Alert the copied text */
     copyText.addEventListener('click', () => {
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
         Swal.fire({
             position: 'top-start',
             icon: 'success',
