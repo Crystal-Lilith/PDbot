4be9ca4251a185042e2dfad9e1caa96c33b80e2a
SKane <le_cas315@hotmail.com>
2020-03-14 23:12:44 +1000

    email.js update

--- web/static/src/js/contact/email.js
+++ web/static/src/js/contact/email.js
@@ -13,6 +13,7 @@ function email_copy() {
             icon: 'success',
             title: 'Copied to clipboard!',
             background: '#1d1e22',
+            padding: '5rem',
             showConfirmButton: false,
             timer: 1500
         })
