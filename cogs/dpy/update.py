533f64b8740a6fe47a5a98981e777866f428e7e9
SKane <le_cas315@hotmail.com>
2020-03-18 21:05:02 +1000

    update.py comment out

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -1,7 +1,7 @@
-@client.command(description='Updates the PDBot||None')
-async def update(ctx):
-    embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
-    embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-    await ctx.channel.send(embed=embed)
-    os.system('./stop.sh')
-    sys.exit()
+# @client.command(description='Updates the PDBot||None')
+# async def update(ctx):
+#     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
+#     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+#     await ctx.channel.send(embed=embed)
+#     os.system('./stop.sh')
+#     sys.exit()
