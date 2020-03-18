167c0bb7a8a75fd2cafb8561e0ae15a197351c0d
SKane <le_cas315@hotmail.com>
2020-03-18 13:47:58 +1000

    dbot.py pagination test

--- dbot.py
+++ dbot.py
@@ -3,9 +3,10 @@ from random import randint
 
 import discord, requests, random, pyfiglet
 from discord.ext import commands
+from discord.ext.commands import Paginator
 from discord.utils import get
 from bs4 import BeautifulSoup
-from flask import Flask, request
+# from flask import Flask, request
 
 client = commands.Bot(command_prefix=os.environ.get('PREFIX'), case_insensitive=True, description='PDBot - v 0.9.0', 
                         status=discord.Status.idle, activity=discord.Game(name='Compiling'))
@@ -27,21 +28,25 @@ async def help(ctx):
                 cmds[command.name] = {"desc": desc[0], 'syntax': desc[-1], 'required_roles': [], 'required_perms': []}
         json.dump(cmds, f)
         f.close()
+    help_menu = Paginator()
+    help_menu.add_line(line='test')
+    await ctx.channel.send(embed=help_menu)
+
 
 for i in os.listdir('./cogs/dpy'):
     if i.endswith('.py'):
-        with open(f"./cogs/dpy/{i}") as f:
+        with open(f"./cogs/dpy/{i}", encoding='utf8') as f:
             exec(f.read())
 
 token = os.environ.get('TOKEN')
 
-app=Flask("Private API")
+# app=Flask("Private API")
 
-@app.route("/guild_count", methods=['GET'])
-def guild_count():
-    if request.method != 'GET':
-        return f'You have to use GET requests not {str(request.method)} requests!'
-    return str(len(client.guilds))
+# @app.route("/guild_count", methods=['GET'])
+# def guild_count():
+#     if request.method != 'GET':
+#         return f'You have to use GET requests not {str(request.method)} requests!'
+#     return str(len(client.guilds))
 
 
 # threading.Thread(target=WSGIServer, args=((('0.0.0.0',9010), app).serve_forever()).start())
