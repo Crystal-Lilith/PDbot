90680aedb634d72a88dc925df1117d05bf0af7ca
SKane <le_cas315@hotmail.com>
2020-03-18 14:36:19 +1000

    dbot.py update

--- dbot.py
+++ dbot.py
@@ -27,16 +27,16 @@ async def help(ctx):
                 cmds[command.name] = {"desc": desc[0], 'syntax': desc[-1], 'required_roles': [], 'required_perms': []}
         json.dump(cmds, f)
         f.close()
-    p = commands.Paginator(prefix='```css')
+    p = commands.Paginator(prefix='```')
     cmds = read_json('cmds/dcmds.json')
     p.add_line('[List of Custom Commands]')
     msg = []
     for cmd in sorted(cmds):
         msg.append(cmd)
     for i in msg:
-        p.add_line(f'{i}\n')
+        p.add_line(i)
     for page in p.pages:
-        embed = discord.Embed(title='[List of commands]', color=discord.Color.from_rgb(0, 191, 255),
+        embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255),
                         description=page)
         embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
         await ctx.channel.send(embed=embed)
