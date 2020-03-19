796cab2aa3668783288a225bc1cadd518989e3e8
SKane <le_cas315@hotmail.com>
2020-03-19 12:25:18 +1000

    dbot.py help menu fix

--- dbot.py
+++ dbot.py
@@ -17,31 +17,32 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name='~$ ./PDBot'))
 
 @client.command()
-async def help(ctx):
-    cmds = {}
-    with open('./cmds/dcmds.json', 'w') as f:
-        for command in client.commands:
-            desc = command.description.split('||')
-            if command.name != 'help':
-                cmds[command.name] = {"desc": desc[0], 'syntax': desc[-1], 'required_roles': [], 'required_perms': []}
-        json.dump(cmds, f)
-        f.close()
+async def help(ctx, *, arguments):
+    if not arguments:
+        cmds = {}
+        with open('./cmds/dcmds.json', 'w') as f:
+            for command in client.commands:
+                desc = command.description.split('||')
+                if command.name != 'help':
+                    cmds[command.name] = {"desc": desc[0], 'syntax': desc[-1], 'required_roles': [], 'required_perms': []}
+            json.dump(cmds, f)
+            f.close()
 
-    cmd_list = []
-    for y in os.listdir('./cmds/'):
-        with open('./cmds/'+y, 'r') as f:
-            x = json.load(f)
-        for i in x:
-            if i.lower() != 'help':
-                cmd_list.append(i)
-    p = commands.Paginator(prefix='```fix')
-    for i in cmd_list:
-        p.add_line(i)
-    for page in p.pages:
-        embed = discord.Embed(title='[List of commands]', color=discord.Color.from_rgb(0, 191, 255),
-                        description=page)
-        embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-        await ctx.channel.send(embed=embed)
+        cmd_list = []
+        for y in os.listdir('./cmds/'):
+            with open('./cmds/'+y, 'r') as f:
+                x = json.load(f)
+            for i in x:
+                if i.lower() != 'help':
+                    cmd_list.append(i)
+        p = commands.Paginator(prefix='```fix')
+        for i in cmd_list:
+            p.add_line(i)
+        for page in p.pages:
+            embed = discord.Embed(title='[List of commands]', color=discord.Color.from_rgb(0, 191, 255),
+                            description=page)
+            embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+            await ctx.channel.send(embed=embed)
 
 
 for i in os.listdir('./cogs/dpy'):
