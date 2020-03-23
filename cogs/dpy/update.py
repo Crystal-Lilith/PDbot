e0a455ebae81e300b08cce492f06f2937dd14e88
SKane <le_cas315@hotmail.com>
2020-03-24 01:27:11 +1000

    update.py fix

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -5,7 +5,7 @@ async def update(ctx):
     await ctx.channel.send(embed=embed)
     try:
         await os.system('clear && ./stop.sh')
-        await client.close()
+        await sys.exit()
     except:
         embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Failed to update the bot!```')
         embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
