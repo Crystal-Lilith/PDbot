ddd57bfbcd5bc3f587feead511c5297341a59293
SKane <le_cas315@hotmail.com>
2020-03-23 14:35:08 +1000

    message edit audit log fix

--- cogs/dpy/audit-log.py
+++ cogs/dpy/audit-log.py
@@ -14,13 +14,13 @@ async def on_message_delete(message):
 
 @client.listen()
 async def on_message_edit(old_message, new_message):
-    log_channel = get(message.guild.channels, name='log')
+    log_channel = get(old_message.guild.channels, name='log')
     try:
         embed = discord.Embed(title='Message Updated ♻️', color=discord.Color.from_rgb(178, 34, 34))
         embed.add_field(name='Old:', value=old_message.content)
         embed.add_field(name='New:', value=new_message.content)
         embed.add_field(name='Channel:', value=old_message.channel.mention)
-        embed.set_footer(text=f'Message updated by: {old_message.author}', icon_url=message.author.avatar_url)
+        embed.set_footer(text=f'Message updated by: {old_message.author}', icon_url=old_message.author.avatar_url)
     except:
         embed = discord.Embed(title='Message Updated ♻️', color=discord.Color.from_rgb(178, 34, 34),
                                 description='Failed to display contents')
