643127188f2e89756dbb14ff518e5c85bafe7a82
SKane <le_cas315@hotmail.com>
2020-03-24 00:29:52 +1000

    update.py debug

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -3,11 +3,11 @@ async def update(ctx):
     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
     await ctx.channel.send(embed=embed)
-    try:
-        await bot.close()
-        await os.system('./stop.sh')
-        await sys.exit()
-    except:
-        embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Failed to update the bot!```')
-        embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-        await ctx.channel.send(embed=embed)
+    # try:
+    await bot.close()
+    await os.system('./stop.sh')
+    await sys.exit()
+    # except:
+    #     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Failed to update the bot!```')
+    #     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+    #     await ctx.channel.send(embed=embed)
