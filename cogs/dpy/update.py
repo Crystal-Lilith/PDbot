f1c0b6de15acd1f9a7f47141d8d493a138913a28
SKane <le_cas315@hotmail.com>
2020-03-23 18:54:11 +1000

    update.py indentation fix

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -3,10 +3,10 @@ async def update(ctx):
     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
     await ctx.channel.send(embed=embed)
-        try:
-            await os.system('./stop.sh')
-            await sys.exit()
-        except:
-            pass
-        finally:
-            await os.system('./start.sh')
+    try:
+        await os.system('./stop.sh')
+        await sys.exit()
+    except:
+        pass
+    finally:
+        await os.system('./start.sh')
