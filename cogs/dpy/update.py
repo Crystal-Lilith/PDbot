50ec884819d5009736441cdb6abf91c6cf7ef7be
DMNight6 <arnokong365@gmail.com>
2020-03-23 15:21:08 +0800

    Update update.py

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -1,12 +1,12 @@
-# @client.command(description='Updates the PDBot||None')
-# async def update(ctx):
-#     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
-#     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
-#     await ctx.channel.send(embed=embed)
-#     try:
-#       os.system('./stop.sh')
-#       sys.exit()
-#     except:
-#       pass
-#     finally:
-#       os.system('./start.sh')
+@client.command(description='Updates the PDBot||None')
+async def update(ctx):
+    embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
+    embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+    await ctx.channel.send(embed=embed)
+        try:
+            await os.system('./stop.sh')
+            await sys.exit()
+        except:
+            pass
+        finally:
+            await os.system('./start.sh')
