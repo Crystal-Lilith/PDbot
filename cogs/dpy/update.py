6e2acf8ccf172048ba41765fa98d5c759a107496
SKane <le_cas315@hotmail.com>
2020-03-23 20:59:44 +1000

    update.py fix

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -3,11 +3,11 @@ async def update(ctx):
     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
     await ctx.channel.send(embed=embed)
-        try:
-            await bot.close()
-            await os.system('./stop.sh')
-            await sys.exit()
-        except:
-            pass
-        finally:
-            await os.system('./start.sh')
+    try:
+        await bot.close()
+        await os.system('./stop.sh')
+        await sys.exit()
+    except:
+        pass
+    finally:
+        await os.system('./start.sh')
