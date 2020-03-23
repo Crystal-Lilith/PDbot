b55713f0d1254ba42c1388d562d94b688bc30d00
DMNight6 <arnokong365@gmail.com>
2020-03-23 22:05:50 +0800

    Update update.py
    Required Futher Testings

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -3,6 +3,7 @@ async def update(ctx):
     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
     await ctx.channel.send(embed=embed)
+<<<<<<< Updated upstream
         try:
             await bot.close()
             await os.system('./stop.sh')
@@ -11,3 +12,13 @@ async def update(ctx):
             pass
         finally:
             await os.system('./start.sh')
+=======
+    try:
+        await os.system('./stop.sh')
+        await sys.exit()
+        await bot.close()
+    except:
+        embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Failed to update the bot!```')
+        embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
+        await ctx.channel.send(embed=embed)
+>>>>>>> Stashed changes
