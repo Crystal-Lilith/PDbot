190b4c09c52e14cf4d1a020c5d0209c8c6846521
Crystal-Lilith <62522566+Crystal-Lilith@users.noreply.github.com>
2020-03-24 13:17:17 +0000

    I just realized that you don't need to export the token and everything because it already gets exported

--- start.sh
+++ start.sh
@@ -4,10 +4,6 @@ if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
-export TOKEN
-export PREFIX
-export CLIENT_ID
-export CLIENT_SECRET
 sudo gem install discordrb dotenv
 while :
 do
