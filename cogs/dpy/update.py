77c253090d82811af34c41d27c3259bec7728154
SKane <le_cas315@hotmail.com>
2020-03-24 17:37:59 +1000

    update.py perms fix

--- cogs/dpy/update.py
+++ cogs/dpy/update.py
@@ -1,4 +1,5 @@
 @client.command(description='Updates the PDBot||None')
+@commands.has_any_role('PDBot Mod', 'PDBot Dev')
 async def update(ctx):
     embed = discord.Embed(color=discord.Color.from_rgb(0, 191, 255), description='```Updating bot...```')
     embed.set_footer(text=f'Requested by: {ctx.message.author}', icon_url=ctx.author.avatar_url)
